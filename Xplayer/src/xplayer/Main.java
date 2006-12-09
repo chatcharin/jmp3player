586b52d010a03415c9f65b1e7456eeb055a3c153
bamboor <bamboor@4c92ea79-191d-0410-8d22-d339660ca4ad>
2006-12-09 13:49:09 +0000

    intian import.

---
+++ Xplayer/src/xplayer/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 9 ธันวาคม 2549, 20:45 น.
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package xplayer;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
